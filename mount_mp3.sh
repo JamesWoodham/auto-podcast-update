46259b2dffe4bc67fbc2b00bcf177d41f15f871c
James Woodham <james@jameswoodham.co.uk>
2016-06-30 13:52:19 +0100

    Update mount_mp3.sh

--- mount_mp3.sh
+++ mount_mp3.sh
@@ -2,12 +2,13 @@
 
 PROJECT_PATH=/home/pi/python-stuff/auto-podcast-update
 SOURCE_PATH=$PROJECT_PATH/audio/*
-DEST_PATH=/media/mp3player/MUSIC/podcasts
+MOUNT_PATH=/media/mp3player
+DEST_PATH=$MOUNT_PATH/MUSIC/podcasts
 
 echo ' ' >>$PROJECT_PATH/log.txt 2>&1
 echo 'mount_mp3.sh executed ' >>$PROJECT_PATH/log.txt 2>&1
 date >>$PROJECT_PATH/log.txt 2>&1
 
-mount /dev/sd*1 /media/mp3player >>$PROJECT_PATH/log.txt 2>&1
+mount /dev/sd*1 $MOUNT_PATH >>$PROJECT_PATH/log.txt 2>&1
 cp -f  $SOURCE_PATH $DEST_PATH >>$PROJECT_PATH/log.txt 2>&1
 rm $SOURCE_PATH >>$PROJECT_PATH/log.txt 2>&1
